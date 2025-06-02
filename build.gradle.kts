83786710cf80c9ec858116bcc46787af8cbb8ed3
VMwareSVGA <xywparrot225@qq.com>
2025-06-02 18:20:06 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.github.megatronking.stringfog:gradle-plugin:5.2.0")
-        classpath("com.github.megatronking.stringfog:xor:5.0.0")
         classpath("com.android.tools.build:gradle:8.2.2")
     }
 }
